277a354e98fceba3c4a4eaa192ab47305eb24a0b
Bajol55 <devarafadil35@gmail.com>
2026-04-06 13:01:00 +0700

    15

--- Fadil.cpp
+++ Fadil.cpp
@@ -5,3 +5,23 @@
     dataku[1] = 200;
     cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
 
+    // mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+[10:41, 06/04/2026] ⚙️ Bayue TI: // menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+
+  
+    
