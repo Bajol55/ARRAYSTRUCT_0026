6225ce93c8af19c81731fe6d4b734dd658634362
Bajol55 <devarafadil35@gmail.com>
2026-04-06 12:48:26 +0700

    4

--- app.cpp
+++ app.cpp
@@ -8,6 +8,14 @@ int main()
 int main() 
 {
     // deklarasi array di sertai dengan inisialisasi
-    int dataku[5] = {10, 20, 30, 40, 50};
+    in  //cara akses (membaca atau mengabil isi array
+    ct dataku[5] = {10, 20, 30, 40, 50};
     //deklarasi array tanpa inisialisasi
     int datamu[5];
+
+  out << "Isi array data kedua: " << dataku[1] << endl;
+    //mengisi array
+    dataku [1] = 200;
+    cout << "Isi array data kedua setelah diubah: " << dataku[1] << endl;
+
+  
