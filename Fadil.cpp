abffd35719b2fe0bc0370a6b79310e05ef4e2984
Bajol55 <devarafadil35@gmail.com>
2026-04-06 12:59:55 +0700

    13

--- Fadil.cpp
+++ Fadil.cpp
@@ -1,4 +1,8 @@
 [10:38, 06/04/2026] ⚙️ Bayue TI: // cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
 
-    
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
+
+  
